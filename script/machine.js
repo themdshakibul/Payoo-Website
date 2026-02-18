f78738843a2eecf0ab1f75d223d0408ead38b444
themdshakibul <mdshakibul88240@gmail.com>
2026-02-19 04:41:35 +0600

    others Buttons added

--- script/machine.js
+++ script/machine.js
@@ -26,16 +26,22 @@ function setBalance(value) {
 function showOnly(id) {
   const addmoney = document.getElementById("add-money");
   const cashout = document.getElementById("cashout");
+  const history = document.getElementById("history");
+  const transformMoney = document.getElementById("transform-money");
+  const getBonus = document.getElementById("get-bonus");
+  const paybill = document.getElementById("pay-bill");
+
   // console.log(`addMoney - ${addmoney} , Cashout ${cashout}`);
 
   // shobaike hide kore dao
   addmoney.classList.add("hidden");
   cashout.classList.add("hidden");
+  history.classList.add("hidden");
+  transformMoney.classList.add("hidden");
+  getBonus.classList.add("hidden");
+  paybill.classList.add("hidden");
 
   // id wala element ta ke tumi show koro
-  const addmoneyH = document.getElementById(id);
-  addmoneyH.classList.remove("hidden");
-
-  const cashoutH = document.getElementById(id);
-  cashoutH.classList.remove("hidden");
+  const selected = document.getElementById(id);
+  selected.classList.remove("hidden");
 }
