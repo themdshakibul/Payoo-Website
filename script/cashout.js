861044b268934e9a64c02009032a21a1e02d6743
themdshakibul <mdshakibul88240@gmail.com>
2026-02-18 09:07:19 +0600

    logic complet

--- script/cashout.js
+++ script/cashout.js
@@ -1,1 +1,9 @@
-console.log("cash out ");
+document.getElementById("cashout-btn").addEventListener("click", function () {
+  // 1 get the agent number & validate
+  // 2 get the amount, validate, convart to Number
+  // 3 get the Current Balance, validate, convart to Number
+  // 4 Calculate new Balance
+  // 5 Get the PI and verify
+  // 5.1 true --> show and alert > set Balance
+  // 5.2 false --> show and error alert > return
+});
